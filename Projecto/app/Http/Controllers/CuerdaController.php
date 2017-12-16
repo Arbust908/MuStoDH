2dfb3c8e78ce440979851257e5b5b2c1ce189d2d
Arbust0 <Arbust908@gmail.com>
2017-12-16 14:37:26 -0300

    Arreglo de Identacion y Tabs
    Fuking OCD

--- Projecto/app/Http/Controllers/CuerdaController.php
+++ Projecto/app/Http/Controllers/CuerdaController.php
@@ -9,44 +9,44 @@ use App\Http\Controllers\Session;
 
 class CuerdaController extends Controller
 {
-     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $cuerdas = Cuerda::All(); 
+		/**
+		* Display a listing of the resource.
+		*
+		* @return \Illuminate\Http\Response
+		*/
+		public function index()
+		{
+			$cuerdas = Cuerda::All();
 
-		return view('categoria-cuerda',compact('cuerdas'));  
-    }
+			return view('categoria-cuerda',compact('cuerdas'));
+		}
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        return view('agregarCuerda');
-    }
+		/**
+		* Show the form for creating a new resource.
+		*
+		* @return \Illuminate\Http\Response
+		*/
+		public function create()
+		{
+			return view('agregarCuerda');
+		}
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-			$reglas = [ 
+		/**
+		* Store a newly created resource in storage.
+		*
+		* @param  \Illuminate\Http\Request  $request
+		* @return \Illuminate\Http\Response
+		*/
+		public function store(Request $request)
+		{
+			$reglas = [
 				"title" => "required|string",
 				"description" => "required|string",
 				"price" =>  "required|numeric|min:0|max:999999",
 				"thumbnail" => "image"
 			];
 
-			$mensajes = [ 
+			$mensajes = [
 				"required" => ":attribute es requerido",
 				"min" => ":attribute tiene un mínimo de :min",
 				"max" => ":attribute tiene un maximo de :max"
@@ -58,63 +58,64 @@ class CuerdaController extends Controller
 
 			$nombreFoto = $foto->storePublicly("public/thumbnails");
 
-			$cuerda = new Cuerda(); 
+			$cuerda = new Cuerda();
 			$cuerda->title = $request["title"];
 			$cuerda->description = $request["description"];
 			$cuerda->price = $request["price"];
 			$cuerda->poster = $nombreFoto;
 
-			$cuerda->save(); 
-            Session::flash('message', 'El instrumento se creo!');
+			$cuerda->save();
+			Session::flash('message', 'El instrumento se creo!');
 
-			return redirect('cuerdas'); 
-    }
+			return redirect('cuerdas');
+			}
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Cuerda  $cuerda
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Cuerda $id)
-    {
-        $cuerda = Cuerda::find($id);
-				return view('cuerda',compact('cuerda')); 
-    }
+		/**
+		* Display the specified resource.
+		*
+		* @param  \App\Cuerda  $cuerda
+		* @return \Illuminate\Http\Response
+		*/
+		public function show(Cuerda $id)
+		{
+			$cuerda = Cuerda::find($id);
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Cuerda  $cuerda
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Cuerda $id)
-    {
-        $cuerda = Cuerda::find($id);
+			return view('cuerda',compact('cuerda'));
+		}
 
-				return view('editarCuerda',compact('cuerda')); 
-    }
+		/**
+		* Show the form for editing the specified resource.
+		*
+		* @param  \App\Cuerda  $cuerda
+		* @return \Illuminate\Http\Response
+		*/
+		public function edit(Cuerda $id)
+		{
+			$cuerda = Cuerda::find($id);
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Cuerda  $cuerda
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Cuerda $id)
-    {
-			$viejo = Cuerda::find($id); 
+			return view('editarCuerda',compact('cuerda'));
+		}
+
+		/**
+		* Update the specified resource in storage.
+		*
+		* @param  \Illuminate\Http\Request  $request
+		* @param  \App\Cuerda  $cuerda
+		* @return \Illuminate\Http\Response
+		*/
+		public function update(Request $request, Cuerda $id)
+		{
+			$viejo = Cuerda::find($id);
 
 
-			$reglas = [ 
+			$reglas = [
 				"title" => "required|string",
 				"description" => "string",
 				"price" =>  "numeric|min:0|max:999999",
 				"thumbnail" => "image"
 			];
 
-			$mensajes = [ 
+			$mensajes = [
 
 				"required" => ":attribute es requerido",
 				"min" => ":attribute tiene un mínimo de :min",
@@ -123,38 +124,38 @@ class CuerdaController extends Controller
 
 			$this->validate($request, $reglas, $mensajes);
 
-            if ($request->file !== NULL) {
-               $foto = $request->file("thumbnail");
+			if ($request->file !== NULL) {
+				$foto = $request->file("thumbnail");
 
-                $nombreFoto = $foto->storePublicly("public/thumbnails");
-                $viejo[0]->poster = $nombreFoto;
-            }
-			
+				$nombreFoto = $foto->storePublicly("public/thumbnails");
+				$viejo[0]->poster = $nombreFoto;
+			}
+
 			$viejo[0]->name = $request["name"];
 			$viejo[0]->description = $request["description"];
 			$viejo[0]->price = $request["price"];
-			
-            dd($viejo);
-			$viejo[0]->save(); 
+
+			dd($viejo);
+			$viejo[0]->save();
 
 
-		return redirect('/categoria-cuerda');
-    }
+			return redirect('/categoria-cuerda');
+		}
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Cuerda  $cuerda
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Cuerda $id)
-    {
+		/**
+		* Remove the specified resource from storage.
+		*
+		* @param  \App\Cuerda  $cuerda
+		* @return \Illuminate\Http\Response
+		*/
+		public function destroy(Cuerda $id)
+		{
 			$cuerda = Cuerda::find($id);
 
 			$cuerda[0]->delete();
 
-            //hacer la prefgunta (si queres borrar)
-          //  Session::flash('message', 'Se borro Pa!!! XD');
-            return redirect('/cuerda');
-    }
+				//hacer la prefgunta (si queres borrar)
+				//  Session::flash('message', 'Se borro Pa!!! XD');
+			return redirect('/cuerda');
+		}
 }
